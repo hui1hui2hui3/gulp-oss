2e34c2c1c43e693b48d53959c9d408a80e93ca42
张少辉 <huis@ACA80145.ipt.aol.com>
2016-03-15 12:00:42 +0800

    增加上传重试参数

--- index.js
+++ index.js
@@ -6,66 +6,77 @@ var mime = require('mime');
 var ALY = require('aliyun-sdk');
 mime.default_type = 'text/plain';
 
-module.exports = function (aws, options) {
-  options = options || {};
+module.exports = function(aws, options) {
+    options = options || {};
+    var maxTries = options.maxRetry || 3;
 
-  if (!options.delay) {
-    options.delay = 0;
-  }
-
-  ALY.config.update({
-    accessKeyId: aws.key,
-    secretAccessKey: aws.secret
-  });
-
-  var oss = new ALY.OSS({
-    endpoint: aws.endpoint,
-    apiVersion: '2013-10-15'
-  });
-
-  var regexGeneral = /\.([a-z]{2,})$/i;
-
-  return es.mapSync(function (file) {
-
-    // Verify this is a file
-    if (!file.isBuffer()) {
-      return file;
+    if (!options.delay) {
+        options.delay = 0;
     }
 
-    var uploadPath = file.path.replace(file.base, options.uploadPath || '');
-    uploadPath = uploadPath.replace(new RegExp('\\\\', 'g'), '/');
+    ALY.config.update({
+        accessKeyId: aws.key,
+        secretAccessKey: aws.secret
+    });
 
-    var headers = {};
-    if (options.headers) {
-      for (var key in options.headers) {
-        headers[key] = options.headers[key];
-      }
-    }
+    var oss = new ALY.OSS({
+        endpoint: aws.endpoint,
+        apiVersion: '2013-10-15'
+    });
 
-    // Set content type based of file extension
-    if (!headers['ContentType'] && regexGeneral.test(uploadPath)) {
-      headers['ContentType'] = mime.lookup(uploadPath);
-    }
+    var regexGeneral = /\.([a-z]{2,})$/i;
 
-    //headers['Content-Length'] = file.stat.size;
+    return es.mapSync(function(file) {
 
-    headers['Body'] = file.contents;
-
-    headers['Key'] = uploadPath;
-
-    oss.putObject(headers,
-      function (err, data) {
-
-        if (err) {
-          gutil.log(gutil.colors.red('[FAILED]', file.path + " -> " + uploadPath));
-          gutil.log(err);
-          return;
+        // Verify this is a file
+        if (!file.isBuffer()) {
+            return file;
         }
 
-        gutil.log(gutil.colors.green('[SUCCESS]', file.path + " -> " + uploadPath));
+        var uploadPath = file.path.replace(file.base, options.uploadPath || '');
+        uploadPath = uploadPath.replace(new RegExp('\\\\', 'g'), '/');
 
-      });
+        var headers = {};
+        if (options.headers) {
+            for (var key in options.headers) {
+                headers[key] = options.headers[key];
+            }
+        }
 
-    return file;
-  });
+        // Set content type based of file extension
+        if (!headers['ContentType'] && regexGeneral.test(uploadPath)) {
+            headers['ContentType'] = mime.lookup(uploadPath);
+        }
+
+        //headers['Content-Length'] = file.stat.size;
+
+        headers['Body'] = file.contents;
+
+        headers['Key'] = uploadPath;
+
+        var tryNum = 1;
+        var upload = function() {
+            oss.putObject(headers,
+                function(err, data) {
+
+                    if (err) {
+                        if (tryNum > maxTries) {
+                            gutil.log(gutil.colors.red('[FAILED]', file.path + " -> " + uploadPath));
+                            gutil.log(err);
+                        } else {
+                            gutil.log(gutil.colors.red('[FAILED RETRY]', file.path + " -> " + uploadPath));
+                            tryNum++;
+                            upload();
+                        }
+                        return;
+                    }
+
+                    gutil.log(gutil.colors.green('[SUCCESS]', file.path + " -> " + uploadPath));
+
+                });
+        };
+        upload();
+
+        return file;
+    });
 };
